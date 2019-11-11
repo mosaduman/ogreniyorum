54d33442cef9b2cbf99f188058a16221201687e4
CihanErsoy <c.ersoy@hotmail.com.tr>
2019-11-11 16:56:20 +0300

    enter the commit message for your changes. Lines starting deneme amaclidir

---
+++ basicjsonpractise.py
@@ -0,0 +1,16 @@
+import json
+
+while True:
+    fname = input("Enter file name: ")
+    fh = open(fname)
+    data = fh.read()
+    print('Retrieved', len(data), 'characters')
+
+    info = json.loads(data)
+    item = info["comments"]
+    print('Count:', len(item))
+    y = 0
+    for newitem in item:
+        x = newitem["count"]
+        y = x + y
+    print('Sum:', y)
